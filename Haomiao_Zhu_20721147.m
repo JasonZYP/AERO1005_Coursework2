e5228ae07231dd09717ee7c9bbabaf27b47d99b4
联想 <@LAPTOP-1PPO7S8F>
2026-04-14 20:56:13 +0800

    Initial commit, add coursework templates

---
+++ Haomiao_Zhu_20721147.m
@@ -0,0 +1,27 @@
+%MATLAB Coursework 2
+%Name: Haomiao Zhu
+%Email Address: ssyhz38@nottingham.edu.cn
+%Student ID: 20721147
+
+
+%% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
+% a) GitHub
+!git --version
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
+% Insert answers here
+
+%% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
+
+% Insert answers here
+
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+
+% Insert answers here
