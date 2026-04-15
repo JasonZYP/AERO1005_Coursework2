f2b1f47ab25869d40652544346b64e8fae765a0c
联想 <@LAPTOP-1PPO7S8F>
2026-04-15 16:25:27 +0800

    I have already downloaded the “MATLAB Support Package for Arduino Hardware”and completed preliminary task, Arduino communication test.

--- Haomiao_Zhu_20721147.m
+++ Haomiao_Zhu_20721147.m
@@ -5,8 +5,40 @@
 
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
-% a) GitHub
+% (a) GitHub
 !git --version
+% Initial commit, add coursework templates.
+
+% (b) MATLAB/Arduino configuration
+% I have already downloaded the "MATLAB Support Package for Arduino
+% Hardware".
+
+% (c) Arduino communication
+% 1. Initialise Arduino connection
+a = arduino('COM7','UNO'); 
+
+% 2. Define parameters
+led_pin = 'D13';
+blink_interval = 0.5;
+loop_times = 10;
+
+% 3. Blink loop
+fprintf('Starting LED blink test\n');
+for i = 1:loop_times
+    % Turn LED ON
+    writeDigitalPin(a, led_pin, 1); 
+    fprintf('Loop %d: LED ON\n',i);
+    pause(blink_interval);
+   
+    % Turn LED OFF
+    writeDigitalPin(a, led_pin, 0);  
+    fprintf('Loop %d: LED OFF\n',i);
+    pause(blink_interval);
+end
+fprintf('LED blink test completed successfully\n');
+
+% Release Arduino port
+clear a;
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
